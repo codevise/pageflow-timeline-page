99e03bc3dda319a4bc384ab04b9678a067fb410a
Tim Fischbach <tfischbach@codevise.de>
2015-09-07 18:43:13 +0200

    Add content text and shadow

--- app/assets/javascripts/pageflow/timeline_page/components/page.jsx
+++ app/assets/javascripts/pageflow/timeline_page/components/page.jsx
@@ -1,17 +1,21 @@
 (function() {
   class Page extends pageflow.react.components.Page {
     render() {
-      var {PageWrapper, PageBackground, PageContent, PageHeader, PageBackgroundImage} = pageflow.react.components;
+      var {PageWrapper,
+           PageBackground, PageBackgroundImage, PageShadow,
+           PageContent, PageHeader, PageText} = pageflow.react.components;
       var {Timeline} = pageflow.timelinePage;
   
       return (
         <PageWrapper>
           <PageBackground>
             <PageBackgroundImage page={this.props.page} />
+            <PageShadow page={this.props.page} />
           </PageBackground>
   
           <PageContent>
             <PageHeader page={this.props.page} />
+            <PageText page={this.props.page} />
             <Timeline pageLinks={this.props.page.pageLinks} />
           </PageContent>
         </PageWrapper>
