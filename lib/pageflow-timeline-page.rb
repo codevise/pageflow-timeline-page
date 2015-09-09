b9fad50ae06917c327f8b5aee4ff9f04fae969a0
Tim Fischbach <tfischbach@codevise.de>
2015-09-09 18:34:00 +0200

    Improve feature/plugin interface

--- lib/pageflow-timeline-page.rb
+++ lib/pageflow-timeline-page.rb
@@ -2,6 +2,10 @@ require 'pageflow/timeline_page/engine'
 
 module Pageflow
   module TimelinePage
+    def self.plugin
+      TimelinePage::Plugin.new
+    end
+
     def self.page_type
       Pageflow::React.create_page_type('timeline_page', 'pageflow.timelinePage.Page')
     end
